86b4a6aab5201012052b166e5b2c9b24e356ea27
mattintosh4 <mattintosh4@gmail.com>
2013-09-24 19:07:16 +0900

    no message

--- wineloader.py
+++ wineloader.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# WINELOADER
+# Nihonshu - Custom Wine Binary for OS X (Ja)
 #
-# Copyright (C) 2013 mattintosh4, https://github.com/mattintosh4
+# Copyright (C) 2013 mattintosh4, https://github.com/mattintosh4/nihonshu
 
 import os
 import sys
