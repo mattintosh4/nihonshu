10af4e2ba04941d3b07b2a6d06b56f437ecef887
mattintosh4 <mattintosh4@gmail.com>
2013-09-27 02:09:26 +0900

    no message

--- wineloader.py
+++ wineloader.py
@@ -4,7 +4,19 @@
 # Nihonshu - Custom Wine Binary for OS X (Ja)
 #
 # Copyright (C) 2013 mattintosh4, https://github.com/mattintosh4/nihonshu
-
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+#
 import os
 import sys
 from subprocess import check_call
