52436290bca706b2a32c39408780c96d709cd5e4
mattintosh4 <mattintosh4@gmail.com>
2013-09-27 17:08:51 +0900

    no message

--- wineloader.py
+++ wineloader.py
@@ -21,27 +21,26 @@ import os
 import sys
 from subprocess import check_call
 
-WINEPREFIX = os.path.expanduser("~/.wine")
-#WINEDEBUG  = "+loaddll"
 LANG       = "ja_JP.UTF-8"
+#WINEDEBUG  = "+loaddll"
 
 ################################################################################
 
 PREFIX      = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 WINE        = os.path.join(PREFIX, "libexec", "wine")
-PLUGINDIR   = os.path.join(PREFIX, 'share/wine/plugin')
+PLUGINDIR   = os.path.join(PREFIX, "share/wine", "plugin")
 
 if sys.argv[1] in ["--help", "--version"]:
     os.execv(WINE, sys.argv)
 
-if "WINEPREFIX" in globals(): os.environ.setdefault("WINEPREFIX", WINEPREFIX)
-if "WINEDEBUG"  in globals(): os.environ.setdefault("WINEDEBUG",  WINEDEBUG)
 if "LANG"       in globals(): os.environ.setdefault("LANG",       LANG)
+if "WINEDEBUG"  in globals(): os.environ.setdefault("WINEDEBUG",  WINEDEBUG)
 
 if "WINEPREFIX" in os.environ:
     WINEPREFIX = os.getenv("WINEPREFIX")
 else:
     WINEPREFIX = os.path.expanduser("~/.wine")
+
 if not os.path.exists(WINEPREFIX):
     os.makedirs(WINEPREFIX)
     import init_wine
