deb4878537976ebd4bda5b157ea9501de7172dc7
mattintosh4 <mattintosh4@gmail.com>
2013-09-30 17:32:48 +0900

    no message

--- wineloader.py
+++ wineloader.py
@@ -20,30 +20,32 @@
 import os
 import sys
 
-PREFIX    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-WINE      = os.path.join(PREFIX, "libexec/wine")
-WINEDEBUG = "" #+ "+loaddll"
+PREFIX      = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+WINE        = os.path.join(PREFIX, "libexec/wine")
+WINEDEBUG   = "" #+ "+loaddll"
 
-if (len(sys.argv) < 2 or
-    sys.argv[1] in ["--help", "--version"]
-):
+
+if len(sys.argv) < 2 or sys.argv[1] in ["--help", "--version"]:
     os.execv(WINE, sys.argv)
+
 
 os.environ.setdefault("LANG", "ja_JP.UTF-8")
 os.environ.setdefault("WINEDEBUG", WINEDEBUG)
 
-wineprefix = os.environ.get("WINEPREFIX", os.path.expanduser("~/.wine"))
-wineprefix = os.path.join(wineprefix, "drive_c")
-wineprefix = os.path.exists(wineprefix)
-if (not wineprefix or
-    sys.argv[1] in ["--skip-init", "--suppress-init", "--force-init"]
-):
+
+wineprefix  = os.environ.get("WINEPREFIX", os.path.expanduser("~/.wine"))
+wineprefix  = os.path.join(wineprefix, "drive_c")
+wineprefix  = os.path.exists(wineprefix)
+with_opt    = sys.argv[1] in ["--skip-init", "--suppress-init", "--force-init"]
+if not wineprefix or with_opt:
     import init_wine
     init_wine.PREFIX    = PREFIX
     init_wine.WINE      = WINE
     init_wine.PLUGINDIR = os.path.join(PREFIX, "share/wine/plugin")
     init_wine.main()
 
+
 print >> sys.stderr, "\033[4;32m" + " ".join(sys.argv[1:]) + "\033[m"
+
 
 os.execv(WINE, sys.argv)
