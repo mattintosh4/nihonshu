4dc4e713d4b20746e6f91846ee209a1cacaf1002
mattintosh4 <mattintosh4@gmail.com>
2013-10-08 02:08:43 +0900

    no message

--- winetricksloader.py
+++ winetricksloader.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-import os
-import sys
-
+import os, sys
 PREFIX     = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 WINETRICKS = os.path.join(PREFIX, 'libexec', 'winetricks')
-
 os.environ['PATH'] = os.path.join(PREFIX, 'bin') + ':' + os.getenv('PATH')
-
 os.execv(WINETRICKS, sys.argv)
